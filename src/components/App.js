3381da94321685a8d9ca96109b8a2562fd19f5f4
Ceejayski <sirchijioke1@gmail.com>
2021-10-05 17:51:37 +0100

    Review Error

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,16 @@ import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
 import store from '../reducers/store';
 
+const wrappedApp = (
+  <Provider store={store}>
+    <BooksList />
+    <BooksForm />
+  </Provider>
+);
+
 const App = () => (
   <div>
-    <Provider store={store}>
-
-      <BooksList />
-      <BooksForm />
-    </Provider>
+    { wrappedApp }
   </div>
 );
 export default App;
